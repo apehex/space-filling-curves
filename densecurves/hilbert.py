253db3795bcd2b5711093a298a5fc05574b6dc08
apehex <apehex@protonmail.com>
2025-07-08 17:43:15 +0200

    Add an option for block curves

--- densecurves/hilbert.py
+++ densecurves/hilbert.py
@@ -2,6 +2,8 @@
 
 import functools
 
+import densecurves.linear
+
 # BINARY #######################################################################
 
 def encode_binary(number: int, width: int) -> str:
@@ -55,7 +57,7 @@ def untangle(coords: list, order: int, rank: int) -> list:
 
 # 1D => 2D #####################################################################
 
-def point(position: int, order: int, rank: int) -> list:
+def _point(position: int, order: int, rank: int) -> list:
     # gray encoding H ^ (H/2)
     __gray = encode_gray(position)
     # approximate the curve
@@ -63,12 +65,36 @@ def point(position: int, order: int, rank: int) -> list:
     # Undo excess work
     return untangle(__coords, order=order, rank=rank)
 
+def point(position: int, order: int, rank: int, group: int=0) -> list:
+    # side of the fine blocks
+    __block = 1 << group
+    # split the index into blocks
+    __coarse, __fine = divmod(position, __block ** rank)
+    # coarse coordinates, following the Hilbert curve
+    __coarse = _point(__coarse, order=order, rank=rank)
+    # fine coordinates, inside each block
+    __fine = densecurves.linear.point(__fine, base=__block, rank=rank)
+    # combine both coordinates systems
+    return [__c * __block + __f for __c, __f in zip(__coarse, __fine)]
+
 # 2D => 1D #####################################################################
 
-def index(coords: list, order: int, rank: int) -> int:
+def _index(coords: list, order: int, rank: int) -> int:
     # entangle the positions back
     __coords = entangle(coords, order=order, rank=rank)
     # flatten the coordinate
     __position = flatten_axes(__coords, order=order, rank=rank)
     # decode the gray encodings
     return decode_gray(__position)
+
+def index(coords: list, order: int, rank: int, group: int=0) -> int:
+    # side of the fine blocks
+    __block = 1 << group
+    # split the coordinates
+    __coarse, __fine = list(zip(*[divmod(__c, __block) for __c in coords]))
+    # coarse index, on the curve
+    __coarse = _index(__coarse, order=order, rank=rank)
+    # fine index, inside the block
+    __fine = densecurves.linear.index(__fine, base=__block, rank=rank)
+    # combine the indexes
+    return __fine + __coarse * (__block ** rank)
